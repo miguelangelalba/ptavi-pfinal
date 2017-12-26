6ad2992816ca9f1cbc7aee3dbdb7bec8d255545b
miguelangel3 <miguelangel.abb@gmail.com>
2017-12-26 18:04:08 +0100

    Implemento métodos, necesito crear servidor para ver si funcionan

--- uaclient.py
+++ uaclient.py
@@ -7,13 +7,66 @@ import sys
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
-etiquetas = {"account":["username","passwd"],
-    "uaserver":["ip","puerto"],
-    "rtpaudio":["puerto"],
-    "regproxy":["ip","puerto"],
-    "log":["path"],
-    "audio":["path"]
+
+etiquetas = {
+    "account": ["username", "passwd"],
+    "uaserver": ["ip", "puerto"],
+    "rtpaudio": ["puerto"],
+    "regproxy": ["ip", "puerto"],
+    "log": ["path"],
+    "audio": ["path"]
 }
+
+
+answer_code = {
+    "Trying": b"SIP/2.0 100 Trying\r\n\r\n",
+    "Ringing": b"SIP/2.0 180 Ringing\r\n\r\n",
+    "Ok": b"SIP/2.0 200 OK\r\n\r\n",
+    "Bad Request": b"SIP/2.0 400 Bad Request\r\n\r\n",
+    "Method Not Allowed": b"SIP/2.0 405 Method Not Allowed\r\n\r\n"
+    }
+
+SIP_type = {
+    "BYE": answer_code["Ok"],
+    "ACK": answer_code["Ok"]
+    }
+
+
+def msg_constructor():
+    u"""Función constructora de mensajes."""
+    if METHOD == "REGSITER":
+        msg = METHOD + " sip:" + login + ":" port + " SIP/2.0" + "\r\n" +
+        "Expires: " + OPTION + "\r\n")
+    elif METHOD == "INVITE":
+        head = METHOD +" sip:" + OPTION + ":" port + " SIP/2.0" + "\r\n"
+        content_type = "content_type: application/sdp" +"\r\n\r\n"
+        v = "v=0"
+        o = "o=" CONF["account_username"] + CONF["userserver_ip"] + "\r\n"
+        s = "s= misesion" + "\r\n"
+        t = "t=0" + "\r\n"
+        m = "m=audio" + CONF["rtpaudio_puerto"] + "RTP" + "\r\n"
+        msg = head + content_type + v + o + s + t + m
+    elif METHOD == "BYE":
+        msg = METHOD + " sip:" + OPTION + "SIP/2.0" + "\r\n"
+
+    return msg
+
+
+def comunication():
+    u"""Comunicación cliente/servidor.
+
+    Creamos el socket, lo configuramos y lo atamos a un servidor/puerto.
+    """
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+
+        my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        my_socket.connect((CONF[regproxy_ip], CONF[regproxy_port]))
+        msg_to_send = msg_constructor()
+        my_socket.send(bytes(msg_to_send, 'utf-8') + b'\r\n')
+        print("Enviando: " + msg_to_send)
+        data = my_socket.recv(1025)
+        print('Recibido -- ', data.decode('utf-8'))
+
 
 class XMLHandler(ContentHandler):
     """Constructor XML"""
@@ -29,7 +82,8 @@ class XMLHandler(ContentHandler):
             return
 
         for atributo in etiquetas[name]:
-            #Con identifico la etiqueta y el argumento en la misma línea
+            #Identifico la etiqueta y el argumento en la misma línea
+            #Lo meto en un diccionario
             self.XML[name + "_" + atributo] = attrs.get(atributo, "")
 
 if __name__ == '__main__':
@@ -53,5 +107,4 @@ if __name__ == '__main__':
     cHandler = XMLHandler()
     parser.setContentHandler(cHandler)
     parser.parse(open(CONFIG))
-    lista = cHandler.get_tags()
-    print(lista)
+    CONF = cHandler.get_tags()
